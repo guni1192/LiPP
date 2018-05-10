941ddb086ff01c580e91ef554a1c668cec8531f4
guni973 <ad2314ce71926@gmail.com>
2018-05-11 00:32:29 +0900

    pass spec

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,12 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe UserController, type: :controller do
-
-  describe "GET #detail" do
-    it "returns http success" do
+RSpec.describe Api::V1::UserController, type: :controller do
+  describe 'GET #detail' do
+    it 'returns http success' do
       get :detail
       expect(response).to have_http_status(:success)
     end
   end
-
 end
