bbc81f5bee178ee15f291c5f1c6baca7590a72d1
guni973 <ad2314ce71926@gmail.com>
2018-05-11 03:13:09 +0900

    add vuetify

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+    <v-app>
+      <v-content>
+        <v-container>Hello world</v-container>
+        <div>
+          <v-btn color="success">Success</v-btn>
+          <v-btn color="error">Error</v-btn>
+          <v-btn color="warning">Warning</v-btn>
+          <v-btn color="info">Info</v-btn>
+        </div>
+      </v-content>
+    </v-app>
   </div>
 </template>
 
--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from '../app.vue'
+
+import ('vuetify/dist/vuetify.min.css')
+import ('material-design-icons-iconfont/dist/material-design-icons.css')
+
+Vue.use(Vuetify)
 
 document.addEventListener('DOMContentLoaded', () => {
   const el = document.body.appendChild(document.createElement('hello'))
