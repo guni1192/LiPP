28bfc4bfc6f7b183d5cf0e48e5bb09d820a46bef
guni973 <ad2314ce71926@gmail.com>
2018-05-11 18:14:41 +0900

    とりあえずuser#detailのテスト消した

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -3,10 +3,4 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UserController, type: :controller do
-  describe 'GET #detail' do
-    it 'returns http success' do
-      get :detail
-      expect(response).to have_http_status(:success)
-    end
-  end
 end
